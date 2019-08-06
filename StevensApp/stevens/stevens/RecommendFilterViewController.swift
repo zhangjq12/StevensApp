9c8928231708dc389b29d269b6305d2f56b30ee3
ShuhaoBai <shuhao.bai@gmail.com>
2019-08-05 22:45:09 -0400

    Category界面Built
    重做了Category，6个Category Filter，准备做drop down menu来选择filter选项

---
+++ StevensApp/stevens/stevens/RecommendFilterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecommendFilterViewController.swift
+//  stevens
+//
+//  Created by Shuhao Bai on 8/5/19.
+//  Copyright © 2019 JqZhang. All rights reserved.
+//
+
+import UIKit
+
+class RecommendFilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
