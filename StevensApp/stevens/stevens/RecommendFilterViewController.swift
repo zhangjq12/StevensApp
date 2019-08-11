612a1b587cf610306de3948639dbd1d6e1e2816b
ShuhaoBai <shuhao.bai@gmail.com>
2019-08-10 20:24:11 -0400

    用Code重建Category View来修复SIGABRT error，保留了drop down menu的code。现在需要往stackview里面加image button

--- StevensApp/stevens/stevens/RecommendFilterViewController.swift
+++ StevensApp/stevens/stevens/RecommendFilterViewController.swift
@@ -9,8 +9,9 @@
 
 import UIKit
 
+/* ==== 这一大块代码是drop-down menu
 class RecommendFilterViewController: UIViewController {
-    
+
     var button = dropDownBtn()
 
     override func viewDidLoad() {
@@ -18,17 +19,17 @@ class RecommendFilterViewController: UIViewController {
         button = dropDownBtn.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
         button.setTitle("Filter Options", for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
-        
+
         self.view.addSubview(button)
         //view.addSubview(button)
-        
-        
+
+
         button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
-        
+
         button.widthAnchor.constraint(equalToConstant: 150).isActive = true // button width
         button.heightAnchor.constraint(equalToConstant: 40).isActive = true // button height
-        
+
         button.dropView.dropDownOptions = ["Most Recent Posts", "Most Viewed Posts"]
         // Do any additional setup after loading the view.
     }
@@ -49,21 +50,21 @@ class dropDownBtn: UIButton, dropDownProtocal{
         self.setTitle(string, for: .normal)
         self.dismissDropDown() // 调用dismissDropDown方程，在选择完选项后关闭下拉菜单
     }
-    
-    
+
+
     var dropView = dropDownView()
     var height = NSLayoutConstraint()
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+
         self.backgroundColor = UIColor.darkGray
-        
+
         dropView = dropDownView.init(frame: CGRect.init(x: 0, y: 0, width: 0, height: 0))
         dropView.delegate = self //because of this line of code, the options that got choosen - the data - will be passed along to the func dropDownPressed
         dropView.translatesAutoresizingMaskIntoConstraints = false
     }
-    
+
     override func didMoveToSuperview() {
         self.superview?.addSubview(dropView)
         self.superview?.bringSubviewToFront(dropView)
@@ -72,33 +73,33 @@ class dropDownBtn: UIButton, dropDownProtocal{
         dropView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true;
         height = dropView.heightAnchor.constraint(equalToConstant: 0)
     }
-    
+
     var isOpen = false
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if isOpen == false {
             isOpen = true
             NSLayoutConstraint.deactivate([self.height])
-            
+
             //消除drop down menu里空白的row
             if self.dropView.tableView.contentSize.height > 150 {
                 self.height.constant = 150
             } else {
                 self.height.constant = self.dropView.tableView.contentSize.height
             }
-            
+
             //self.height.constant = 150
-            
-            
+
+
             NSLayoutConstraint.activate([self.height])
             UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
                 self.dropView.layoutIfNeeded()
                 self.dropView.center.y += self.dropView.frame.height / 2
                 }, completion: nil)
-            
-            
+
+
         } else {
             isOpen = false
-            
+
             NSLayoutConstraint.deactivate([self.height])
             self.height.constant = 0
             NSLayoutConstraint.activate([self.height])
@@ -109,7 +110,7 @@ class dropDownBtn: UIButton, dropDownProtocal{
         }
     }
     func dismissDropDown(){//选完选项之后关闭下拉菜单
-        
+
         isOpen = false
         NSLayoutConstraint.deactivate([self.height])
         self.height.constant = 0
@@ -126,36 +127,36 @@ class dropDownBtn: UIButton, dropDownProtocal{
 
 
 class dropDownView: UIView, UITableViewDelegate, UITableViewDataSource {
-    
+
     var dropDownOptions = [String]()
-    
+
     var tableView = UITableView()
-    
+
     var delegate : dropDownProtocal! //create a delegate part for our drop down view
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+
         tableView.backgroundColor = UIColor.darkGray
         self.backgroundColor = UIColor.darkGray
-        
+
         tableView.delegate = self
         tableView.dataSource = self
-        
+
         tableView.translatesAutoresizingMaskIntoConstraints = false
-        
+
         self.addSubview(tableView)
-        
+
         tableView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
         tableView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
         tableView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -168,10 +169,13 @@ class dropDownView: UIView, UITableViewDelegate, UITableViewDataSource {
         cell.backgroundColor = UIColor.darkGray
         return cell
     }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.delegate.dropDownPressed(string: dropDownOptions[indexPath.row])
         self.tableView.deselectRow(at: indexPath, animated: true)
     }
-    
+
 }
+*/
+
+
